7d50536432ce0e278ceff4cd4d3b99cc52dcdc67
Joshua Khan <Khanjo@gmail.com>
2023-09-08 10:07:36 -0700

    add files for components and WIP header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <React.Fragment>
+            <h1>&#127861; What A Teas &#9749;</h1>
+        </React.Fragment>
+    );
+}
+
+export default Header;
