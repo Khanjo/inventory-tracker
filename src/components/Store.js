db7b29ec8c7d95cca09dd369899a69f702a05496
Joshua Khan <Khanjo@gmail.com>
2023-09-08 13:22:09 -0700

    allow user to add items to inventory

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Inventory from "./Inventory";
 import NewItemForm from "./NewItemForm";
+import EditItemForm from "./EditItemForm";
 
 class Store extends React.Component {
 
@@ -15,22 +16,43 @@ class Store extends React.Component {
     }
 
     handleClick = () => {
-        this.setState({ newItemForm: true });
+        if (this.state.selectedItem != null) {
+            this.setState({
+                newItemForm: false,
+                selectedItem: null,
+                editItemForm: false
+            });
+        } else {
+            this.setState(prevState => ({
+                newItemForm: !prevState.newItemForm
+            }));
+        }
+    }
+
+    handleAddingItem = (newItem) => {
+        const newMainItemList = this.state.mainItemList.concat(newItem);
+        this.setState({ mainItemList: newMainItemList, newItemForm: false });
     }
 
     render() {
         let currentlyVisibleState = null;
-        let addItemButton = null;
-        if (this.state.newItemForm) {
-            currentlyVisibleState = <NewItemForm />
+        let buttonText = null;
+        if (this.state.editItemForm) {
+            currentlyVisibleState = <EditItemForm item={this.state.selectedItem} onEditItem={this.handleEditingItem} />;
+            buttonText = "Return to inventory";
+        } else if (this.state.selectedItem != null) {
+
+        } else if (this.state.newItemForm) {
+            currentlyVisibleState = <NewItemForm onNewItemCreation={this.handleAddingItem} />;
+            buttonText = "Return to inventory";
         } else {
-            currentlyVisibleState = <Inventory itemList={this.state.mainItemList} />
-            addItemButton = <button onClick={this.handleClick}>Add Item</button>
+            currentlyVisibleState = <Inventory itemList={this.state.mainItemList} onItemSelection={this.handleSelectedTicket} />;
+            buttonText = "Add Item"
         }
         return (
             <React.Fragment>
                 {currentlyVisibleState}
-                {addItemButton}
+                <button onClick={this.handleClick}>{buttonText}</button>
             </React.Fragment>
         )
     }
